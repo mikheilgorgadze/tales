c5fe015bc801985171d4cfdad5f29ae3978b1748
Misho <mishogorgadze41@gmail.com>
2024-02-21 01:48:55 +0400

    changed styles

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// let image = document.getElementById("nala-image");
+// let button = document.getElementById("next-button");
+// let resized = false;
+// let originalWidth = image.clientWidth;
+// let originalHeight = image.clientHeight;
+// button.addEventListener('click', function(){
+//         if (!resized) {
+//             image.style.height = '400px';
+//             image.style.width = '400px';
+//             resized = true;
+//         }
+//         else {
+//             image.style.height = originalHeight + 'px';
+//             image.style.width = originalWidth + 'px';
+//             console.log(originalHeight, originalWidth);
+//             resized = false;
+//         }
+//     })
