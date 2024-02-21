b62ae58523405a9978e09d4fd03edb6b2f1bf704
Misho <mishogorgadze41@gmail.com>
2024-02-22 01:45:19 +0400

    commented js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-let taleElements = document.querySelectorAll(".tale-element");
-let button = document.getElementById("next-button");
-let clicked = true;
-
-button.addEventListener('click', function (){
-    if (clicked) {
-        for (let i = 0; i < taleElements.length; i++) {
-            taleElements[i].style.display = 'none';
-        }
-        clicked = false;
-    }
-    else {
-        for (let i = 0; i < taleElements.length; i++) {
-            taleElements[i].style.display = 'block';
-        }
-        clicked = true;
-    }
-})
+// let taleElements = document.querySelectorAll(".tale-element");
+// let button = document.getElementById("next-button");
+// let clicked = true;
+//
+// button.addEventListener('click', function (){
+//     if (clicked) {
+//         for (let i = 0; i < taleElements.length; i++) {
+//             taleElements[i].style.display = 'none';
+//         }
+//         clicked = false;
+//     }
+//     else {
+//         for (let i = 0; i < taleElements.length; i++) {
+//             taleElements[i].style.display = 'block';
+//         }
+//         clicked = true;
+//     }
+// })
